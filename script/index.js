8e8c2b023df3f30e881e80e16d04ebb41bd76d15
Dina Telyakova <dinaatel@gmail.com>
2020-12-24 23:12:49 +0300

    The project after second CR

--- script/index.js
+++ script/index.js
@@ -5,26 +5,27 @@ let formElement = document.querySelector('.popup__form');
 let saveButton = popup.querySelector('.popup__safety');
 let nameInput = document.querySelector('.popup__text_type_name');
 let descriptionInput = document.querySelector('.popup__text_type_descripion');
+let descriptionProfile = document.querySelector('.profile__description');
+let nameProfile = document.querySelector('.profile__name');
 
 function openPopup(){
     popup.classList.remove('popup_disabled');
-    nameInput.getAttribute('value');
-    descriptionInput.getAttribute('value');
+    nameInput.textContent = nameProfile.value;
+    descriptionInput.textContent = descriptionProfile.value;
 }
-editButton.addEventListener('click', openPopup);
 
 function closePopup(){
-    popup.classList.add('popup_disabled')
+    popup.classList.add('popup_disabled');
 }
-closeButton.addEventListener('click', closePopup);
 
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    let nameProfile = document.querySelector('.profile__name');
-    let descriptionProfile = document.querySelector('.profile__description');
     nameProfile.textContent = nameInput.value;
     descriptionProfile.textContent = descriptionInput.value;   
 }
+
 formElement.addEventListener('submit', handleFormSubmit);
 saveButton.addEventListener('click', closePopup);
+closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopup);
